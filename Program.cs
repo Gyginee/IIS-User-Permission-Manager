15ed7137133811abf466864e10d8c3de992a6bb0
Gyginee <hungnguyenminh66@gmail.com>
2023-12-17 16:01:25 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+
+class Program : Form
+{
+    private FileSystemWatcher watcher;
+    private NotifyIcon trayIcon;
+    private ListBox messageList;
+    private Button clearButton;
+    private FolderBrowserDialog folderBrowserDialog;
+
+    public Program()
+    {
+        InitializeComponent();
+        InitializeFormComponents();
+        SetupFileSystemWatcher();
+    }
+
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Program));
+            this.SuspendLayout();
+            // 
+            // Program
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Name = "Program";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+    }
+
+    private void InitializeFormComponents()
+    {
+        this.Size = new System.Drawing.Size(400, 300);
+
+        // Setup tray icon
+        trayIcon = new NotifyIcon
+        {
+            Icon = new Icon(@"Icon.ico"),
+            Visible = true
+        };
+        trayIcon.DoubleClick += (sender, args) =>
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        };
+
+        // Setup message list
+        messageList = new ListBox
+        {
+            Dock = DockStyle.Fill
+        };
+        this.Controls.Add(messageList);
+
+        // Setup clear button
+        clearButton = new Button
+        {
+            Text = "Clear Messages",
+            Dock = DockStyle.Bottom
+        };
+        clearButton.Click += (sender, args) => messageList.Items.Clear();
+        this.Controls.Add(clearButton);
+
+        // Setup folder browser dialog
+        folderBrowserDialog = new FolderBrowserDialog();
+    }
+
+    private void Form_Load(object sender, EventArgs e)
+    {
+        // Form Load actions can be handled here if needed
+    }
+
+    private void SetupFileSystemWatcher()
+    {
+        string path = @"C:\DEPLOY"; // Default path
+
+        if (!Directory.Exists(path))
+        {
+            MessageBox.Show("Default directory not found. Please select a directory.","NOT FOUND", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //AppendMessage("Default directory not found. Please select a directory.");
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowserDialog.SelectedPath;
+                this.BringWindowToFront(); // Bring the form to the front after folder selection
+            }
+            else
+            {
+                AppendMessage("No directory selected. File watcher not started.");
+                return;
+            }
+        }
+
+        watcher = new FileSystemWatcher
+        {
+            Path = path,
+            IncludeSubdirectories = true,
+            NotifyFilter = NotifyFilters.FileName,
+            Filter = "*.*"
+        };
+
+        watcher.Created += OnCreated;
+        watcher.EnableRaisingEvents = true;
+
+        AppendMessage($"Watching directory: {path}");
+    }
+
+
+    private void OnCreated(object source, FileSystemEventArgs e)
+    {
+        try
+        {
+            if (File.Exists(e.FullPath))
+            {
+                UpdateFileAccess(e.FullPath);
+            }
+        }
+        catch (Exception ex)
+        {
+            AppendMessage("Error updating permissions: " + ex.Message);
+        }
+    }
+
+
+    private void UpdateFileAccess(string filePath)
+    {
+        FileInfo fileInfo = new FileInfo(filePath);
+        FileSecurity fileSecurity = fileInfo.GetAccessControl();
+        FileSystemAccessRule accessRule = new FileSystemAccessRule("IIS_IUSRS", FileSystemRights.FullControl, AccessControlType.Allow);
+
+        fileSecurity.AddAccessRule(accessRule);
+        fileInfo.SetAccessControl(fileSecurity);
+
+        AppendMessage($"Permissions updated for file: {fileInfo.Name}");
+    }
+
+    private void AppendMessage(string message)
+    {
+        if (this.InvokeRequired)
+        {
+            this.Invoke(new Action(() => messageList.Items.Add(message)));
+        }
+        else
+        {
+            messageList.Items.Add(message);
+        }
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);
+        if (this.WindowState == FormWindowState.Minimized)
+        {
+            this.Hide();
+        }
+    }
+    private void BringWindowToFront()
+    {
+        // Check if we are running on the UI thread
+        if (this.InvokeRequired)
+        {
+            this.Invoke(new Action(BringWindowToFront));
+            return;
+        }
+
+        this.TopMost = true; // Make the form topmost
+        this.Focus(); // Focus on the form
+        this.BringToFront(); // Bring form to the front
+        this.TopMost = false; // Set back TopMost to false
+    }
+    [STAThread]
+    static void Main(string[] args)
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Program());
+    }
+}
---
+++ readme.md
@@ -0,0 +1,45 @@
+# IIS User Permission Manager
+
+## Introduction
+This Windows Forms application is designed to automate the process of granting IIS User permissions on files within a specified directory. It's particularly useful in scenarios where an API pushes images or other files to a server folder, and IIS needs read access to these files.
+
+## Features
+- **Folder Selection**: Users can select any folder for monitoring.
+- **Automatic Permission Granting**: Automatically grants IIS User permissions on new files added to the monitored folder.
+- **Real-Time Monitoring**: Uses `FileSystemWatcher` to monitor the folder in real time.
+- **User-Friendly Interface**: Offers a simple and intuitive graphical interface for ease of use.
+- **Tray Icon Integration**: Minimizes to the system tray for unobtrusive operation.
+
+
+
+```bash
+git clone https://github.com/gyginee/IISUserPermissionManager.git
+cd IISUserPermissionManager
+// Follow any additional installation instructions if applicable
+```
+
+## Usage
+
+After installation, follow these steps to use the application:
+
+1. **Start the Application**: Run the program.
+2. **Choose a Folder**: Use the Folder Browser to select the folder you want to monitor.
+3. **Monitor the Folder**: The application will start monitoring the folder and automatically grant IIS User permissions to any new files.
+4. **View Logs**: The application logs all actions in the interface, allowing you to see which files have been modified.
+
+## Contributing
+
+Contributions to the IIS User Permission Manager are welcome! Here's how you can contribute:
+
+- Fork the repository and create your branch from `master`.
+- Add or improve features, fix bugs, or enhance documentation.
+- Write meaningful commit messages.
+- Submit a pull request with a comprehensive description of changes.
+
+## License
+
+This project is licensed under the MIT License - see the `LICENSE.md` file for details.
+
+## Copyright (c) 2023 [Gyginee]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
